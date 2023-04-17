6b3a88c75b7330278b83b8aff1114f74d1bba9fd
Polina <polina.degtyareva.99@gmail.com>
2023-04-18 00:51:54 +0300

    feat: finish count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let result = matrix.flat().filter(cat => cat === '^^');
+  return result.length
 }
 
-module.exports = {
+module.exports = {  
   countCats
 };
